353d5d537cf3c90a0b7a34ef3b25668dafe0860a
pythonicnerd <vishalmaurya3112@gmail.com>
2022-10-12 21:40:21 +0530

    inital commit for gatsby project start

---
+++ src/pages/test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export default function Test() {
+  return (
+    <main>
+      <title>Test</title>
+      <h1>Test</h1>
+      <p>Test</p>
+      <Link to="/">Go home</Link>
+    </main>
+  );
+}
